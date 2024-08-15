d0aa7157da5927c19a3042b4e0f7717d9dd11af9
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-08-14 21:47:14 -0700

    Tweeter button invoke login modal

--- src/app/components/SidebarTweetButton.tsx
+++ src/app/components/SidebarTweetButton.tsx
@@ -1,22 +1,30 @@
-import React from 'react'
+'use client'
+import React, { useCallback } from 'react'
 import Link from 'next/link'
 import { FaFeather } from 'react-icons/fa'
+import useLoginModal from '../hooks/useLoginModal'
 
 const SidebarTweetButton = () => {
-  return (
-    <div>
-        <Link href='/'>
-            <div className='mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer'>
-                <FaFeather size={24} color="white" />
-            </div>
-            <div className='mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-90 transition cursor-pointer'>
-                <p className='hidden lg:block text-center font-semibold text-white text-[20px]'>
-                    Tweet
-                </p>
-            </div>
-        </Link>
-    </div>
-  )
+
+    const loginModal = useLoginModal()
+
+    const onClick = useCallback(() => {
+        loginModal.onOpen()
+    }, [loginModal])
+    return (
+        <div onClick={onClick}>
+            {/* <Link href='/'> */}
+                <div className='mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer'>
+                    <FaFeather size={24} color="white" />
+                </div>
+                <div className='mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-90 transition cursor-pointer'>
+                    <p className='hidden lg:block text-center font-semibold text-white text-[20px]'>
+                        Tweet
+                    </p>
+                </div>
+            {/* </Link> */}
+        </div>
+    )
 }
 
 export default SidebarTweetButton

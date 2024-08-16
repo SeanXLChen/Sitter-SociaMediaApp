51a21a55f356dd3282a9f48bde1a93b46109b8f2
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-08-16 14:21:36 -0700

    Add following and follower design to user page

--- src/app/components/users/UserBio.tsx
+++ src/app/components/users/UserBio.tsx
@@ -4,6 +4,7 @@ import useUser from "@/app/hooks/useUser"
 import useCurrentUser from "@/app/hooks/useCurrentUser"
 
 import Button from "../Button"
+import { BiCalendar } from "react-icons/bi"
 
 
 interface UserBioProps {
@@ -24,16 +25,60 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
 
   return (
     <div className="border-b-[1px] border-neutral-800 pb-4">
+
       <div className="flex justify-end p-2">
-        
         {/* if user is current user, can edit profile, otherwise, can follow */}
         {currentUser?.id === userId ? (
           <Button secondary label="Edit" onClick={() => { }} />
         ) : (
           <Button secondary label="Follow" onClick={() => { }} />
         )}
+      </div>
+
+      <div className="mt-8 px-4">
+        <div className="flex flex-col">
+          <p className="text-white text-2xl font-semibold">
+            {fetchedUser?.name}
+          </p>
+          <p className="text-md text-neutral-500">
+            @{fetchedUser?.username}
+          </p>
+        </div>
+        <div className="flex flex-col mt-4">
+          <p className="text-white">
+            {fetchedUser?.bio}
+          </p>
+          <div className="flex flex-row items-center gap-2 mt-4 text-neutral-500">
+            <BiCalendar size={24} />
+            <p> Joined {createdAt}</p>
+          </div>
+
+        </div>
+
+        <div className="flex flex-row items-center mt-4 gap-6">
+          <div className=" flex flex-row items-center gap-1">
+            <p className="text-white">
+              {fetchedUser?.followingIds?.length}
+            </p>
+            <p className="text-neutral-500">
+              Following
+            </p>
+
+          </div>
+          <div className=" flex flex-row items-center gap-1">
+            <p className="text-white">
+              {fetchedUser?.followersCount || 0}
+            </p>
+            <p className="text-neutral-500">
+              Followers
+            </p>
+
+          </div>
+        </div>
+
 
       </div>
+
     </div>
   )
 }

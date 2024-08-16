6d5cb3e040701145c8728ac5b1af4a03a27c1e6f
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-08-16 11:46:52 -0700

    Create condition button render for viewing self or viewing other user

--- src/app/components/users/UserBio.tsx
+++ src/app/components/users/UserBio.tsx
@@ -1,5 +1,10 @@
+import { format } from "date-fns"
+import { useMemo } from "react"
 import useUser from "@/app/hooks/useUser"
 import useCurrentUser from "@/app/hooks/useCurrentUser"
+
+import Button from "../Button"
+
 
 interface UserBioProps {
   userId: string
@@ -8,9 +13,28 @@ interface UserBioProps {
 const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   const { data: currentUser } = useCurrentUser()
   const { data: fetchedUser } = useUser(userId)
-  
+
+  const createdAt = useMemo(() => {
+    if (!fetchedUser?.createdAt) {
+      return null;
+    }
+
+    return format(new Date(fetchedUser.createdAt), "MMMM yyyy")
+  }, [fetchedUser?.createdAt])
+
   return (
-    <div>UserBio</div>
+    <div className="border-b-[1px] border-neutral-800 pb-4">
+      <div className="flex justify-end p-2">
+        
+        {/* if user is current user, can edit profile, otherwise, can follow */}
+        {currentUser?.id === userId ? (
+          <Button secondary label="Edit" onClick={() => { }} />
+        ) : (
+          <Button secondary label="Follow" onClick={() => { }} />
+        )}
+
+      </div>
+    </div>
   )
 }
 

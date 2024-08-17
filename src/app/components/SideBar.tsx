bbf5eb0d932b7c059d4209da82faa2963660a67d
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-08-16 17:21:19 -0700

    Fix: profile redirect to user's profile page

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -28,7 +28,7 @@ const Sidebar = () => {
         },
         {
             label: 'Profile',
-            href: '/users/0001',
+            href: '/users/${currentUser?.id}',
             icon: FaUser,
             auth: true
         }
